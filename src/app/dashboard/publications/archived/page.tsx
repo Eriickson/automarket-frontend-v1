89b5db204aebc431494e475ed3e1ecb322020773
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-10-29 09:46:53 -0400

    Adding publications routes

---
+++ src/app/dashboard/publications/archived/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArchivedPublicationsPage = () => {
+  return <div>ArchivedPublicationsPage</div>;
+};
+
+export default ArchivedPublicationsPage;
---
+++ src/app/dashboard/publications/published/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PublishedPublicationsPage = () => {
+  return <div>PublishedPublicationsPage</div>;
+};
+
+export default PublishedPublicationsPage;
