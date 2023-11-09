dea61c0f786df6c152fece8d23dadc38523a14aa
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-11-09 16:14:08 -0400

    Add moment.js library and update package-lock.json

--- src/app/signup/register-email/components/view/steps/RegisterPersonalInformation/schema.ts
+++ src/app/signup/register-email/components/view/steps/RegisterPersonalInformation/schema.ts
@@ -2,9 +2,28 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { z } from "zod";
 
+import moment from "moment";
+
 const registerPersonalInformationSchema = z.object({
-  fullname: z.string().min(1, "Este campo es requerido"),
-  birthday: z.string().min(1, "Este campo es requerido"),
+  // fullname: z
+  //   .string()
+  //   .min(1, "Este campo es requerido")
+  //   .regex(/^([0-9]{2})-([0-9]{2})-([0-9]{4})$/, "El formato de fecha es dd-mm-yyyy")
+  //   .refine(
+  //     (data) => {
+  //       const date = moment(data, "DD-MM-YYYY");
+  //       return date.isValid();
+  //     },
+  //     { message: "La fecha no es válida" }
+  //   )
+  //   .refine(
+  //     (data) => {
+  //       const date = moment(data, "DD-MM-YYYY");
+  //       return moment().diff(date, "years") >= 18;
+  //     },
+  //     { message: "Debes ser mayor de edad" }
+  //   ),
+  // birthday: z.string().min(1, "Este campo es requerido"),
 });
 
 export const resolver = zodResolver(registerPersonalInformationSchema);
