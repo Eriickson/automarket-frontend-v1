3fbc9c35c93887dfafa504dd1d9c1203cf2b4f2f
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-11-13 14:56:28 -0400

    Add agency and branch endpoints to types

---
+++ src/@types/endpoints/agencies.d.ts
@@ -0,0 +1,9 @@
+declare module "@atmk/endpoints" {
+  export type AGENCIES_ENDPOINTS =
+    | "CHANGE_LOGO"
+    | "CHANGE_COVER"
+    | "UPDATE_AGENCY_PROFILE"
+    | "CREATE_AN_AGENCY"
+    | "UPDATE_SOCIAL_MEDIA"
+    | "CHECK_FIELD_AVAILABILITY";
+}
---
+++ src/@types/endpoints/branches.d.ts
@@ -0,0 +1,10 @@
+declare module "@atmk/endpoints" {
+  export type BRANCHES_ENDPOINTS =
+    | "UPDATE_SCHEDULE"
+    | "UPDATE_CONTACTS"
+    | "UPDATE_ADDRESS"
+    | "CHANGE_MAIN_BRANCH"
+    | "GET_CONTACTS"
+    | "CREATE_BRANCH"
+    | "GET_SCHEDULE";
+}
