c52021fb38acbb80e377abb338b230888a2c7797
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-11-13 15:05:53 -0400

    Rearrange steps and update agency form layout

--- src/app/create-an-account/components/view/steps/CreateAnAgency/index.tsx
+++ src/app/create-an-account/components/view/steps/CreateAnAgency/index.tsx
@@ -1,19 +1,21 @@
-import { SimpleTextFieldController } from "@/components/atoms/SimpleTextField/controller";
+import React from "react";
+
 import {
   AspectRatio,
   Box,
   Button,
   FormControl,
-  FormHelperText,
   FormLabel,
-  HStack,
   Heading,
+  HStack,
   Image,
   Input,
   Stack,
   Text,
 } from "@chakra-ui/react";
-import React from "react";
+
+import { SimpleTextFieldController } from "@/components/atoms/SimpleTextField/controller";
+
 import { FormProvider, useForm } from "react-hook-form";
 import { useSwiper } from "swiper/react";
 
@@ -29,34 +31,34 @@ export const CreateAnAgencyStep = () => {
   return (
     <Box>
       <Box mb="8">
-        <Heading as="h2" size="xl" fontWeight="semibold">
+        <Heading as="h2" fontWeight="semibold" size="xl">
           Datos de la agencia
         </Heading>
         <Text>Estos serán los datos que aparecerán en tu perfil de agencia.</Text>
       </Box>
       <FormProvider {...methods}>
-        <Stack as="form" onSubmit={methods.handleSubmit(handleSubmit)} spacing="4">
+        <Stack as="form" spacing="4" onSubmit={methods.handleSubmit(handleSubmit)}>
           <FormControl>
             <FormLabel mb="1">Logo</FormLabel>
             <HStack>
               <Box>
-                <AspectRatio w="40" ratio={4 / 3}>
+                <AspectRatio ratio={4 / 3} w="40">
                   <Image
-                    src="https://dmuvf4m79e02w.cloudfront.net/_1200x630_crop_center-center_82_none/ICON_Default_Social_Image_Template.jpg?mtime=1668894453 "
                     alt=""
+                    src="https://dmuvf4m79e02w.cloudfront.net/_1200x630_crop_center-center_82_none/ICON_Default_Social_Image_Template.jpg?mtime=1668894453 "
                   />
                 </AspectRatio>
               </Box>
             </HStack>
           </FormControl>
-          <SimpleTextFieldController name="name" label="Nombre" placeholder="Ingresa el nombre de tu agencia" />
-          <SimpleTextFieldController name="name" label="Eslogan" placeholder="Ingresa el eslogan" />
+          <SimpleTextFieldController label="Nombre" name="name" placeholder="Ingresa el nombre de tu agencia" />
+          <SimpleTextFieldController label="Eslogan" name="name" placeholder="Ingresa el eslogan" />
           <FormControl>
             <FormLabel mb="1">Provincia</FormLabel>
             <Input
               _focus={{ ringColor: "#165ef0", borderColor: "#165ef0", ring: "1.5" }}
-              py="6"
               placeholder="Ingresa el provincia"
+              py="6"
             />
           </FormControl>
           <HStack spacing="4">
@@ -64,21 +66,21 @@ export const CreateAnAgencyStep = () => {
               <FormLabel mb="1">Municipio</FormLabel>
               <Input
                 _focus={{ ringColor: "#165ef0", borderColor: "#165ef0", ring: "1.5" }}
-                py="6"
                 placeholder="Ingresa el municipio"
+                py="6"
               />
             </FormControl>
             <FormControl>
               <FormLabel mb="1">Sector</FormLabel>
               <Input
                 _focus={{ ringColor: "#165ef0", borderColor: "#165ef0", ring: "1.5" }}
-                py="6"
                 placeholder="Ingresa el sector"
+                py="6"
               />
             </FormControl>
           </HStack>
-          <SimpleTextFieldController name="reference" label="Referencía" placeholder="Ingresa la referencia" />
-          <Button py="6" w="full" colorScheme="primary">
+          <SimpleTextFieldController label="Referencía" name="reference" placeholder="Ingresa la referencia" />
+          <Button colorScheme="primary" py="6" w="full">
             Validar datos de la agencia
           </Button>
         </Stack>
