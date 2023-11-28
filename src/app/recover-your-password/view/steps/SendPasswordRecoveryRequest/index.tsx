58fdea01c24bbfe073fcce2d276b2dfb3720d557
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-11-28 14:45:55 -0400

    Add AutoGeneratedAddressText component to CreateAnAgency form

--- src/app/recover-your-password/view/steps/SendPasswordRecoveryRequest/index.tsx
+++ src/app/recover-your-password/view/steps/SendPasswordRecoveryRequest/index.tsx
@@ -1,16 +1,14 @@
-import React from "react";
+import React, { FC } from "react";
 
 import delay from "delay";
-import { useSwiper } from "swiper/react";
 
 import { SendPasswordRecoveryRequestForm } from "./form";
 
-export const SendPasswordRecoveryRequest = () => {
-  const swiperSlide = useSwiper();
+interface SendPasswordRecoveryRequestProps {}
 
+export const SendPasswordRecoveryRequest: FC<SendPasswordRecoveryRequestProps> = () => {
   async function handleSubmit() {
     await delay(1000);
-    swiperSlide.slideNext();
   }
 
   return <SendPasswordRecoveryRequestForm defaultValues={{ email: "erickson01d@gmail.com" }} onSubmit={handleSubmit} />;
