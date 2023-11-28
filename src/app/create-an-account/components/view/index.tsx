21d922d1df37a7aac4083c6a43ba7bbe7e9ebdd0
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-11-28 09:51:20 -0400

    Refactor RegisterEmailView component

--- src/app/create-an-account/components/view/index.tsx
+++ src/app/create-an-account/components/view/index.tsx
@@ -5,9 +5,5 @@ import { Box } from "@chakra-ui/react";
 import { SwiperSteps } from "./swiper";
 
 export const RegisterEmailView = () => {
-  return (
-    <Box maxW="xl">
-      <SwiperSteps />
-    </Box>
-  );
+  return <SwiperSteps />;
 };
