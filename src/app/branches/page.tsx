261362ae68dc5a2b26be1637c0716c88fe894072
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-04 17:13:02 -0400

    Add new pages for agency, account, ads, branches, events, and publications

---
+++ src/app/branches/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const BranchesPage = () => {
+  return <div>BranchesPage</div>;
+};
+
+export default BranchesPage;
+// https://automarket.agency/account
---
+++ src/app/publications/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PublicationsPage = () => {
+  return <div>PublicationsPage</div>;
+};
+
+export default PublicationsPage;
+// https://automarket.agency/publications
+// https://automarket.agency/ads
+// https://automarket.agency/events
