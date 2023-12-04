df633982bc87f23aad9fc553a8721d040d1fd9f1
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-04 17:05:44 -0400

    Refactor dashboard page component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,70 +1,9 @@
 import React from "react";
 
-import { Avatar, Box, Flex, HStack, Image, List, ListItem, Stack, Text } from "@chakra-ui/react";
-
-import { Info } from "react-feather";
+import { Box } from "@chakra-ui/react";
 
 const DashboardPage = () => {
-  return (
-    <Stack h="100vh" maxH="100vh" spacing="4">
-      <Box borderBottom="1px" borderColor="gray.300" px="24" py="4">
-        <Flex alignItems="center" justifyContent="space-between">
-          <Box cursor="pointer">
-            <Image alt="Logo" src="/assets/logo.svg" userSelect="none" />
-          </Box>
-          <Box>
-            <Box _hover={{ bgColor: "gray.100" }} cursor="pointer" px="4" py="2" rounded="sm" transition="150ms">
-              <HStack>
-                <Avatar bgColor="gray.400" name="EM" />
-                <Box userSelect="none">
-                  <Text fontWeight="bold">Erickson Manuel Holguín</Text>
-                  <Text color="gray.500" fontSize="sm" fontWeight="medium">
-                    Administrador
-                  </Text>
-                </Box>
-              </HStack>
-            </Box>
-          </Box>
-        </Flex>
-      </Box>
-      <HStack alignItems="flex-start" h="full" spacing="4">
-        <Box h="full" pl="24" w="md">
-          <List>
-            <Stack>
-              <ListItem _hover={{ bgColor: "gray.200" }} cursor="pointer" px="6" py="4" rounded="sm" transition="150ms">
-                <HStack>
-                  <Info />
-                  <Text fontWeight="semibold">Dashboard</Text>
-                </HStack>
-              </ListItem>
-              <ListItem bgColor="gray.200" cursor="pointer" px="6" py="4" rounded="sm">
-                <HStack color="primary.500">
-                  <Info />
-                  <Text fontWeight="semibold">Dashboard</Text>
-                </HStack>
-              </ListItem>
-              <ListItem cursor="pointer" px="6" py="4" rounded="sm">
-                <HStack>
-                  <Info />
-                  <Text fontWeight="semibold">Dashboard</Text>
-                </HStack>
-              </ListItem>
-              <ListItem cursor="pointer" px="6" py="4" rounded="sm">
-                <HStack>
-                  <Info />
-                  <Text fontWeight="semibold">Dashboard</Text>
-                </HStack>
-              </ListItem>
-            </Stack>
-          </List>
-        </Box>
-        <Box border="1px" borderColor="gray.200" flex="1" h="full" mr="24">
-          Hola
-        </Box>
-      </HStack>
-      <Box />
-    </Stack>
-  );
+  return <Box>Hola a todos</Box>;
 };
 
 export default DashboardPage;
