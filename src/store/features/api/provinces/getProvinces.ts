ff65a9060a7fe3b6e400c9396a6d05b27a5c01f0
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-14 08:32:50 -0400

    Refactor address fields and provinces API
    This commit refactors the address fields component in the CreateAnAgency form and the getProvinces API. It introduces new functions for handling province and municipality changes, improves the select field controllers, and updates the options for provinces, municipalities, and sectors. This improves the user experience and ensures accurate data retrieval.

--- src/store/features/api/provinces/getProvinces.ts
+++ src/store/features/api/provinces/getProvinces.ts
@@ -1,5 +1,12 @@
 import { EndpointBuilderType } from "@atmk/core";
 
+interface IProvince {
+  value: string;
+  label: string;
+}
+
 export const getProvinces = (build: EndpointBuilderType<"provincesApi">) => {
-  return build.query<{ data: any; message: string }, null>({ query: () => ({ url: "/", method: "GET" }) });
+  return build.query<{ data: { provinces: IProvince[] }; message: string }, null>({
+    query: () => ({ url: "/", method: "GET" }),
+  });
 };
