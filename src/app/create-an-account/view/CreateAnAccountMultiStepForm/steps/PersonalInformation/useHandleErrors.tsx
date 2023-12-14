4bdb86b255984353ade85f08f284580b5a67c83d
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-14 12:14:28 -0400

    Refactor error handling in useHandleErrors.tsx

--- src/app/create-an-account/view/CreateAnAccountMultiStepForm/steps/PersonalInformation/useHandleErrors.tsx
+++ src/app/create-an-account/view/CreateAnAccountMultiStepForm/steps/PersonalInformation/useHandleErrors.tsx
@@ -1,31 +1,18 @@
-import { Box, Center, HStack, Stack, Text, useToast } from "@chakra-ui/react";
-
-import { Check } from "react-feather";
+import { useToast } from "@chakra-ui/react";
 
 export const useHandleErrors = () => {
   const toast = useToast();
 
   async function handleErrors(response: { error: unknown }) {
+    const data = response.error as any;
+
     toast({
-      title: "Cantidad máxima de intentos alcanzada",
-      description: "Por favor, espera unos minutos y vuelve a intentarlo.",
+      title: data.data.message,
+      description: data.data.detail,
       status: "error",
       duration: 5000,
       isClosable: true,
       position: "top-right",
-      // render: ({ title, description }) => (
-      //   <HStack alignItems="flex-start" bg="white" border="1px" borderColor="gray.200" borderRadius="sm" px="4" py="2">
-      //     <Center bgColor="red.500" color="white" p="1.5" rounded="full">
-      //       <Check size="1.25rem" strokeWidth="2.5" />
-      //     </Center>
-      //     <Box>
-      //       <Text fontSize="sm" fontWeight="bold">
-      //         {title}
-      //       </Text>
-      //       <Text fontSize="sm">{description}</Text>
-      //     </Box>
-      //   </HStack>
-      // ),
     });
   }
 
