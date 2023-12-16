4196c4233974bd1ce7a418ec48521d058854455c
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-16 15:25:11 -0400

    Add createDealer endpoint to dealersApi

--- src/store/features/api/dealers/index.ts
+++ src/store/features/api/dealers/index.ts
@@ -1,9 +1,12 @@
 import { axiosBaseQuery } from "@/store/baseQueries";
 
+import { createDealer } from "./create-dealer";
 import { createApi } from "@reduxjs/toolkit/query";
 
 export const dealerApi = createApi({
   baseQuery: axiosBaseQuery({ module: "/dealers" }),
-  reducerPath: "dealerApi",
-  endpoints: (build) => ({}),
+  reducerPath: "dealersApi",
+  endpoints: (build) => ({
+    createDealer: createDealer(build),
+  }),
 });
