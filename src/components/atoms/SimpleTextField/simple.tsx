f51fd4b1397c0c002958554b3d479d983c41e56b
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-19 14:41:15 -0400

    Add focus border to SimpleTextField component

--- src/components/atoms/SimpleTextField/simple.tsx
+++ src/components/atoms/SimpleTextField/simple.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 
 import { Box, Input, InputProps } from "@chakra-ui/react";
 
@@ -9,6 +9,8 @@ export interface SimpleTextFieldProps extends InputProps {
 }
 
 export const SimpleTextField = forwardRef<HTMLElement, SimpleTextFieldProps>(({ isLoading, ...props }, ref) => {
+  const [isFocus, setIsFocus] = useState(false);
+
   return (
     <Box position="relative">
       <Input
@@ -23,9 +25,25 @@ export const SimpleTextField = forwardRef<HTMLElement, SimpleTextFieldProps>(({ 
         rounded="sm"
         userSelect={isLoading ? "none" : "auto"}
         {...props}
+        bgColor="white"
         isRequired={false}
         ref={ref}
+        zIndex="1"
+        // onBlur={() => setIsFocus(false)}
+        // onFocus={() => setIsFocus(true)}
       />
+      {isFocus ? (
+        <Box
+          borderColor="primary.700"
+          borderStyle="dotted"
+          borderWidth="2px"
+          inset="-1.5"
+          pos="absolute"
+          rounded="md"
+          zIndex="0"
+        />
+      ) : null}
+
       {isLoading ? (
         <Box position="absolute" right="4" top="3">
           <ReactLoading color="#cdcdcd" type="bars" width="1.75rem" />
@@ -36,3 +54,4 @@ export const SimpleTextField = forwardRef<HTMLElement, SimpleTextFieldProps>(({ 
 });
 
 SimpleTextField.displayName = "SimpleTextField";
+// 2px dotted rgb(91, 105, 135)
