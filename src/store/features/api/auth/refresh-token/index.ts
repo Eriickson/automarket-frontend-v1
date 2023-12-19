25781986108fde24f9cb4f35ce1631edac2fc67e
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-19 11:02:39 -0400

    Refactor refreshToken to use mutation instead of query

--- src/store/features/api/auth/refresh-token/index.ts
+++ src/store/features/api/auth/refresh-token/index.ts
@@ -6,7 +6,7 @@ import { ENDPOINTS } from "@/utils";
 import { RefreshTokenArgsType, RefreshTokenReturnType } from "./types";
 
 export const refreshToken = (build: EndpointBuilderType<"authApi">) => {
-  return build.query<ServiceResponse<RefreshTokenReturnType>, RefreshTokenArgsType>({
+  return build.mutation<ServiceResponse<RefreshTokenReturnType>, RefreshTokenArgsType>({
     query: ({ headers }) => ({ url: ENDPOINTS.AUTH.REFRESH_TOKEN, method: "POST", headers }),
   });
 };
