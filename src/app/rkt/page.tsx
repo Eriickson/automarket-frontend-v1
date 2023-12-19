18b929fcd9a30746862ffa61d42318160a865958
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-19 07:35:14 -0400

    Remove changes

--- src/app/rkt/page.tsx
+++ src/app/rkt/page.tsx
@@ -3,15 +3,7 @@ import React from "react";
 
 import { Box } from "@chakra-ui/react";
 
-import { authApi } from "@/store/features/api/auth";
-
 const RktPage = () => {
-  const { data } = authApi.useCheckFieldAvailabilityQuery({
-    field: "email",
-    value: "erickson01d@gmail.com",
-  });
-
-
   return <Box m="48">RktPage - </Box>;
 };
 
