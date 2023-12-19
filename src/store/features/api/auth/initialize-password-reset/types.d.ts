792437b7a83a38870e43cbe06e1f9d5d51bb4e51
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-19 10:07:59 -0400

    Refactor initializePasswordReset function signature

---
+++ src/store/features/api/auth/initialize-password-reset/types.d.ts
@@ -0,0 +1,10 @@
+export type InitializePasswordResetArgsType = {
+  data: {
+    identifier: string;
+    mode: string;
+  };
+};
+
+export type InitializePasswordResetReturnType = {
+  identifier: string;
+};
