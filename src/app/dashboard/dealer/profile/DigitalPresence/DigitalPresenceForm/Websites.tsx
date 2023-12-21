97995f4db2bcc2b3f2d99aca3a8c6d9d5da17878
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-21 12:39:53 -0400

    Refactor WebsitesPanel component to use react-hook-form

--- src/app/dashboard/dealer/profile/DigitalPresence/DigitalPresenceForm/Websites.tsx
+++ src/app/dashboard/dealer/profile/DigitalPresence/DigitalPresenceForm/Websites.tsx
@@ -1,7 +1,64 @@
 import React from "react";
 
-import { TabPanel } from "@chakra-ui/react";
+import { Box, HStack, List, ListItem, TabPanel, Text } from "@chakra-ui/react";
+
+import { Button, IconButton, SimpleTextFieldController } from "@/components/atoms";
+
+import { Globe, Type, X } from "react-feather";
+import { useFieldArray } from "react-hook-form";
 
 export const WebsitesPanel = () => {
-  return <TabPanel>WebsitesPanel</TabPanel>;
+  const { append, fields, remove } = useFieldArray({ name: "emails" });
+
+  return (
+    <TabPanel>
+      <List spacing="4">
+        {fields.map((field, i) => (
+          <ListItem key={field.id}>
+            <HStack>
+              <Box flex="1" pos="relative">
+                <Box bgColor="blackAlpha.200" bottom="2.5" color="black" p="1" position="absolute" rounded="sm">
+                  <Type size="1rem" strokeWidth="2" />
+                </Box>
+                <SimpleTextFieldController
+                  _focus={{ ring: 0 }}
+                  border="0"
+                  borderBottom="2px"
+                  fontSize="sm"
+                  name={`emails.${i}.title`}
+                  pl="8"
+                  placeholder="Nombre del sitio web"
+                  px="0"
+                  py="1"
+                />
+              </Box>
+              <Text fontSize="xl">:</Text>
+              <Box flex="1" pos="relative">
+                <Box bgColor="blackAlpha.200" bottom="2.5" color="black" p="1" position="absolute" rounded="sm">
+                  <Globe size="1rem" strokeWidth="2.5" />
+                </Box>
+                <SimpleTextFieldController
+                  _focus={{ ring: 0 }}
+                  border="0"
+                  borderBottom="2px"
+                  fontSize="sm"
+                  name={`emails.${i}.value`}
+                  pl="8"
+                  placeholder="Enlace del sitio web"
+                  px="0"
+                  py="1"
+                />
+              </Box>
+              <IconButton aria-label="" rounded="sm" size="xs" onClick={() => remove(i)}>
+                <X size="1rem" strokeWidth="2" />
+              </IconButton>
+            </HStack>
+          </ListItem>
+        ))}
+        <Button isDisabled={fields.length === 3} size="sm" onClick={() => append({})}>
+          Agregar Sitio Web
+        </Button>
+      </List>
+    </TabPanel>
+  );
 };
