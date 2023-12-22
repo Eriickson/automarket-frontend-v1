9afe72539aefda2d67506c4dd0b348b2e40b5acf
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-22 09:00:28 -0400

    Add ProfileSession component to AccountSettingsView

--- src/app/dashboard/settings/view.tsx
+++ src/app/dashboard/settings/view.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Box, HStack, Stack, Text } from "@chakra-ui/react";
 
 import { DashboardSessionTitle } from "../DashboardSessionTitle";
+import { ProfileSession } from "../dealer/profile/ProfileSession";
 export const AccountSettingsView = () => {
   return (
     <HStack alignItems="flex-start" h="full">
@@ -13,7 +14,26 @@ export const AccountSettingsView = () => {
           title="Configuraciones"
         />
         <Stack mt="16" spacing="12">
-          <Text>fodsadas</Text>
+          <Box>
+            <Text>
+              Dispositivo: <b>Mackbook</b>
+            </Text>
+            {/* <Text>
+              Sistema operativo: <b>MacOS</b>
+            </Text> */}
+            {/* <Text>
+              Navegador: <b>Chrome</b>
+            </Text> */}
+            {/* <Text>
+              Ubicación: <b>Santiago de los Caballeros, Rep. Dom.</b>
+            </Text>
+            <Text>
+              Última Actividad: <b>Hace 25 min.</b>
+            </Text>
+            <Text>
+              Versión: <b>92.0.4515.131</b>
+            </Text> */}
+          </Box>
         </Stack>
       </Box>
       <Box h="full" w="md" />
