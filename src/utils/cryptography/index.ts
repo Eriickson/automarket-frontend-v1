5f29186747517fb4196f313779b04cfdf5999a18
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-23 20:53:56 -0400

    Add cryptography utility functions

---
+++ src/utils/cryptography/index.ts
@@ -0,0 +1,31 @@
+import crypto from "crypto";
+
+const key = "a1!b2#c3@d4e5f6g7h8i9j0klmno_pqr";
+const algorithm = "aes-256-ctr";
+const ivLength = 16;
+
+export function encryptData<T = unknown>(data: T, key: string) {
+  const iv = crypto.randomBytes(ivLength);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(JSON.stringify(data));
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  const encryptedData = iv.toString("hex") + ":" + encrypted.toString("hex");
+
+  return encryptedData;
+}
+
+export function decryptData<T = unknown>(encryptedData: string, key: string): T {
+  const textParts = encryptedData.split(":");
+
+  const iv = Buffer.from(textParts.shift() || "", "hex");
+
+  const encryptedText = Buffer.from(textParts.join(":"), "hex");
+
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  let decrypted = decipher.update(encryptedText);
+
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+  return JSON.parse(decrypted.toString());
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export * from "./cryptography";
 export * from "./endpoints";
 export * from "./generateDynamicEndpoint";
 export * from "./getClientIp";
