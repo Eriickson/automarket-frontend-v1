71bcd6dd51bc81668fa8105e5627b92ff367f75e
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-25 08:20:55 -0400

    Refactor GeneralInformationStep component

--- src/app/dashboard/publications/create/view/steps/GeneralInformation/index.tsx
+++ src/app/dashboard/publications/create/view/steps/GeneralInformation/index.tsx
@@ -1,9 +1,27 @@
 import React, { FC } from "react";
 
+import { Box, HStack, Stack } from "@chakra-ui/react";
+
+import { Button } from "@/components/atoms";
+import { SelectField } from "@/components/organisms";
 import { WizardFormItemComponentProps } from "@/components/organisms/WizardForm";
 
 interface GeneralInformationStepProps extends WizardFormItemComponentProps {}
 
 export const GeneralInformationStep: FC<GeneralInformationStepProps> = () => {
-  return <div>GeneralInformationStep</div>;
+  return (
+    <Box>
+      <Stack>
+        <Box>
+          <SelectField options={[]} />
+        </Box>
+        <HStack>
+          <Button bgColor="gray.100" variant="ghost">
+            Volver atrás
+          </Button>
+          <Button colorScheme="primary">Siguiente Apartado</Button>
+        </HStack>
+      </Stack>
+    </Box>
+  );
 };
