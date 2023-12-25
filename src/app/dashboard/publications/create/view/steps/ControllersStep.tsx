d87224d121b2d2a9f217769ec6f53d41fdfa538d
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-25 11:13:54 -0400

    Update UI components and form logic

--- src/app/dashboard/publications/create/view/steps/ControllersStep.tsx
+++ src/app/dashboard/publications/create/view/steps/ControllersStep.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, Center, Checkbox, HStack, ListItem, Radio, Text, UnorderedList } from "@chakra-ui/react";
 
 import { Button } from "@/components/atoms";
 
@@ -13,6 +13,78 @@ interface ControllersStepProps {
 export const ControllersStep: FC<ControllersStepProps> = ({ disablePrevStep, formId, onPrevStep }) => {
   return (
     <Box>
+      <Box mb="4">
+        <UnorderedList spacing="4">
+          <ListItem>
+            <Text fontWeight="semibold">¿El vehículo tiene aire acondicionado?</Text>
+            <HStack>
+              <Radio colorScheme="secondary">
+                <Text>Sí</Text>
+              </Radio>
+              <Radio colorScheme="secondary">
+                <Text>No</Text>
+              </Radio>
+            </HStack>
+          </ListItem>
+          <ListItem>
+            <Text fontWeight="semibold">Bolsas de aires</Text>
+            <HStack>
+              <Checkbox colorScheme="secondary">
+                <Text>Todas</Text>
+              </Checkbox>
+              <Checkbox colorScheme="secondary">
+                <Text>Conductor</Text>
+              </Checkbox>
+              <Checkbox colorScheme="secondary">
+                <Text>Pasajero</Text>
+              </Checkbox>
+              <Checkbox colorScheme="secondary">
+                <Text>Traseras</Text>
+              </Checkbox>
+              <Checkbox colorScheme="secondary">
+                <Text>Laterales</Text>
+              </Checkbox>
+            </HStack>
+          </ListItem>
+          <ListItem>
+            <Text fontWeight="semibold">Mecanismo de los cristales</Text>
+            <HStack>
+              <Radio colorScheme="secondary">
+                <Text>Automáticos</Text>
+              </Radio>
+              <Radio colorScheme="secondary">
+                <Text>Manuales</Text>
+              </Radio>
+            </HStack>
+          </ListItem>
+          <ListItem>
+            <Text fontWeight="semibold">Condiciones de las Llantas</Text>
+            <HStack>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
+                const isActive = item === 5;
+
+                return (
+                  <Center
+                    bgColor={isActive ? "secondary.500" : "white"}
+                    border="1px"
+                    borderColor="gray.400"
+                    color={isActive ? "white" : "secondary.500"}
+                    cursor="pointer"
+                    h="8"
+                    key={item}
+                    rounded="sm"
+                    w="8"
+                  >
+                    <Text fontSize="sm" fontWeight="semibold">
+                      {item}
+                    </Text>
+                  </Center>
+                );
+              })}
+            </HStack>
+          </ListItem>
+        </UnorderedList>
+      </Box>
       <HStack>
         <Button
           bgColor="gray.100"
