8dafbbac875f596dbb317754b9393aba44adb883
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-26 12:45:33 -0400

    Update dealer information session avatar

--- src/app/publications/[uuid]/sessions/DealerInformation/index.tsx
+++ src/app/publications/[uuid]/sessions/DealerInformation/index.tsx
@@ -7,10 +7,10 @@ export const DealerInformationSession = () => {
   return (
     <HStack alignItems="flex-start" spacing="6">
       <HStack>
-        <Avatar src="https://img.supercarros.com/CustomersLogos/100x90/0/55563.jpg" />
+        <Avatar src="" />
         <Box>
           <Text fontWeight="medium" lineHeight="1.2">
-            Electric Motors Showroom
+            Agency Name
           </Text>
           <Text color="gray.500" fontSize="sm" maxW="72" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
             Los mejores vehículos a los mejores precios
