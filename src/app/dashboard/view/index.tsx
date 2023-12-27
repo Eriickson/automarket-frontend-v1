74821b5a93581043f9b06950d2ee52e136de391e
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-26 21:59:56 -0400

    Add DashboardView component

--- src/app/dashboard/view/index.tsx
+++ src/app/dashboard/view/index.tsx
@@ -3,6 +3,5 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 
 export const DashboardView = () => {
-
-  return <Box>Dashboard view</Box>;
+  return <Box>DashboardView</Box>;
 };
