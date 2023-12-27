ff313627f8394e2e3a5d29e9f75a1c1966398753
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-26 21:54:28 -0400

    Refactor file upload component to accept specific file types

--- src/app/sandbox/upload-file/ButtonComponent.tsx
+++ src/app/sandbox/upload-file/ButtonComponent.tsx
@@ -7,13 +7,8 @@ import { UploadFileWrapper } from "./UploadFileWrapper";
 
 export const ButtonComponent = () => {
   return (
-    <UploadFileWrapper allowMultiple={false}>
-      {({ isDragActive }) => (
-        <Button>
-          Upload file
-          {isDragActive ? "true" : "false"}
-        </Button>
-      )}
+    <UploadFileWrapper acceptedFileTypes={["IMAGES", "VIDEOS"]} allowMultiple={false}>
+      {({ isDragActive }) => <Button>Upload file {isDragActive ? "true" : "false"}</Button>}
     </UploadFileWrapper>
   );
 };
--- src/app/sandbox/upload-file/UploadFileWrapper.tsx
+++ src/app/sandbox/upload-file/UploadFileWrapper.tsx
@@ -5,9 +5,18 @@ import { Box } from "@chakra-ui/react";
 
 import { useDropzone } from "react-dropzone";
 
+export type AcceptedFileTypes = "IMAGES" | "PDF" | "VIDEOS";
+
+export const acceptedFileTypesMapper: Record<AcceptedFileTypes, any> = {
+  IMAGES: { "images/*": [".png", ".jpg", ".jpeg", ".gif"] },
+  PDF: { "application/pdf": [".pdf"] },
+  VIDEOS: { "video/*": [".mp4", ".avi", ".mov"] },
+};
+
 interface UploadFileWrapperProps {
   maxFiles?: number;
   allowMultiple?: boolean;
+  acceptedFileTypes?: AcceptedFileTypes[];
   onChanges?(nv: File[]): void;
   children: (props: { isDragActive: boolean }) => JSX.Element;
 }
@@ -17,6 +26,7 @@ export const UploadFileWrapper: FC<UploadFileWrapperProps> = ({
   maxFiles,
   children,
   onChanges,
+  acceptedFileTypes,
 }) => {
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
@@ -29,6 +39,11 @@ export const UploadFileWrapper: FC<UploadFileWrapperProps> = ({
     multiple: allowMultiple,
     onDrop,
     maxFiles,
+    accept: (() => {
+      const objectFilesToAccept = {};
+      acceptedFileTypes?.forEach((fileType) => Object.assign(objectFilesToAccept, acceptedFileTypesMapper[fileType]));
+      return objectFilesToAccept;
+    })(),
   });
 
   return (
