b649fe44efe89886bf48d4aa072154104e4b729f
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-27 21:45:00 -0400

    Add mobile navigation menu with drawer component

--- src/components/organisms/GlobalHeader/MobileNavigationMenu/index.tsx
+++ src/components/organisms/GlobalHeader/MobileNavigationMenu/index.tsx
@@ -1,13 +1,52 @@
 import React from "react";
 
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+
 import { IconButton } from "@/components/atoms";
 
 import { Menu } from "react-feather";
 
 export const MobileNavigationMenu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef(null);
+
   return (
-    <IconButton aria-label="IconButton" bgColor="gray.200" colorScheme="secondary" size="sm" variant="outline">
-      <Menu size="1rem" />
-    </IconButton>
+    <>
+      <IconButton
+        aria-label="IconButton"
+        bgColor="gray.200"
+        colorScheme="secondary"
+        size="sm"
+        variant="outline"
+        onClick={onOpen}
+      >
+        <Menu size="1rem" />
+      </IconButton>
+      <Drawer finalFocusRef={btnRef} isOpen={isOpen} placement="right" size="full" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody></DrawerBody>
+
+          <DrawerFooter>
+            <Button mr={3} variant="outline" onClick={onClose}>
+              Cancel
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
