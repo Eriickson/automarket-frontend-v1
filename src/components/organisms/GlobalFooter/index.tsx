e62612cc023b5d1bea9cf16c47027532a31c22d4
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-29 12:41:45 -0400

    Remove unnecessary code and reorder components in GlobalFooter

--- src/components/organisms/GlobalFooter/index.tsx
+++ src/components/organisms/GlobalFooter/index.tsx
@@ -15,7 +15,6 @@ export const GlobalFooter = () => {
   return (
     <Stack px={[...generateNull(4), "4"]} spacing="4">
       <Divider borderColor="gray.300" />
-
       <ScreenAreaDelimiter>
         <Stack direction={["column"]} mb="4" spacing={["4", null, null, "6"]} w="full">
           <Flex alignItems="center" justifyContent="space-between">
@@ -24,8 +23,8 @@ export const GlobalFooter = () => {
           </Flex>
         </Stack>
         <Stack spacing={["8"]}>
-          <RightSession />
           <OverviewMenu />
+          <RightSession />
         </Stack>
         <Divider borderColor="gray.300" my="4" />
         <Box pb={["3"]}>
