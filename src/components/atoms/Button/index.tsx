938c3310c8fa423052a87995239cf37c1693adcc
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-29 11:20:56 -0400

    Refactor Button and HeaderSession components

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -3,11 +3,13 @@ import React, { FC } from "react";
 
 import { Button as ChakraButton, ButtonProps as ChakraButtonProps } from "@chakra-ui/react";
 
+import { generateNull } from "@/utils";
+
 export interface ButtonProps extends ChakraButtonProps {}
 
 export const Button: FC<ButtonProps> = ({ children, ...props }) => {
   return (
-    <ChakraButton fontSize={["xs"]} rounded="sm" {...props}>
+    <ChakraButton rounded="sm" size={["sm", ...generateNull(5), "md"]} {...props}>
       {children}
     </ChakraButton>
   );
--- src/components/layouts/FullScreen/HeaderSession.tsx
+++ src/components/layouts/FullScreen/HeaderSession.tsx
@@ -2,14 +2,18 @@ import React from "react";
 
 import { Box, Flex } from "@chakra-ui/react";
 
+import { Button } from "@/components/atoms";
 import { LogoPresentation } from "@/components/atoms/LogoPresentation";
 import { generateNull } from "@/utils";
 
 export const HeaderSession = () => {
   return (
-    <Box border="1px" h={["16", ...generateNull(5), "20"]} px={["2", ...generateNull(5), "6"]}>
-      <Flex alignItems="center" h="full">
+    <Box h={["16", ...generateNull(5), "20"]} px={["2", ...generateNull(5), "6"]}>
+      <Flex alignItems="center" h="full" justifyContent="space-between">
         <LogoPresentation />
+        <Button colorScheme="primary" variant="link">
+          Crea tu cuenta
+        </Button>
       </Flex>
     </Box>
   );
