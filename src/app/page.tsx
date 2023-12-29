aee99b8ac9412d1f6f05b2b145f041a7d85bff46
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-29 11:01:02 -0400

    Add EcommerceLayout to HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import EcommerceLayout from "./legacy/legal/layout";
 import { HomeView } from "./view";
 
 const HomePage = () => {
-  return <HomeView />;
+  return (
+    <EcommerceLayout>
+      <HomeView />
+    </EcommerceLayout>
+  );
 };
 
 export default HomePage;
