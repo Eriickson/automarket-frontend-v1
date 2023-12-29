345b6bae41e6b8d528050883fa340fdafb372e44
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-29 14:09:57 -0400

    Remove unused import and comment out ScreenSize component

--- src/components/organisms/providers/index.tsx
+++ src/components/organisms/providers/index.tsx
@@ -5,7 +5,6 @@ import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
-import { ScreenSize } from "@/components/atoms";
 import { useStore } from "@/store/useStore.store";
 import { primaryColor } from "@/themes/color";
 import { mainTheme } from "@/themes/mainTheme";
@@ -31,7 +30,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         <ChakraProvider theme={mainTheme}>
           {isRendered ? children : null}
           <ProgressBar shallowRouting color={primaryColor[500]} height="4px" options={{ showSpinner: true }} />
-          <ScreenSize />
+          {/* <ScreenSize /> */}
         </ChakraProvider>
       </CacheProvider>
     </ReduxProvider>
