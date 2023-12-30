25add33ff89fa0a4557c503b10832b669bfaf4a4
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-12-30 08:18:13 -0400

    Update navbar links and display

--- src/components/organisms/GlobalHeader/NavBar.tsx
+++ src/components/organisms/GlobalHeader/NavBar.tsx
@@ -5,8 +5,8 @@ import { Box, HStack } from "@chakra-ui/react";
 import { NavbarItem } from "./NavbarItem";
 
 const navbarItems = [
-  { label: "Buscar Publicaciones", href: "/searcher/publications" },
-  { label: "Encontrar Distribuidores", href: "/searcher/agencies" },
+  { label: "Buscar Publicaciones", href: "/search" },
+  { label: "Encontrar Distribuidores", href: "/search/dealers" },
   { label: "Explorar", href: "/explore-and-discover" },
   { label: "Planes y Precios", href: "/plans-and-pricing" },
   { label: "Dashboard", href: "/dashboard" },
@@ -14,7 +14,7 @@ const navbarItems = [
 
 export const GlobalHeaderNavBar = () => {
   return (
-    <Box display={["none", null, null, null]}>
+    <Box display={["none", null, null, null, "block"]}>
       <HStack>
         {navbarItems.map((item, i) => (
           <NavbarItem item={item} key={i} />
