542fd19cebb93d078c5d202799620c9a5b1dcceb
Erickson Manuel Holguin <erickson01d@gmail.com>
2024-01-11 19:57:00 -0400

    Add swiper functionality to CreateAnAccountContainer

--- src/app/create-an-account/view/components/CreateAnAccount/index.tsx
+++ src/app/create-an-account/view/components/CreateAnAccount/index.tsx
@@ -1,8 +1,18 @@
 "use client";
 import React from "react";
 
+import { useSwiper } from "swiper/react";
+
 import { CreateAnAccountHolder } from "./CreateAnAccountHolder";
 
 export const CreateAnAccountContainer = () => {
-  return <CreateAnAccountHolder onSubmit={async () => {}} />;
+  const swiper = useSwiper();
+  return (
+    <CreateAnAccountHolder
+      onSubmit={async (data) => {
+        console.log(data);
+        swiper.slideNext();
+      }}
+    />
+  );
 };
