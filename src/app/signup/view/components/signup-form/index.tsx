1254d1a623ac1bab60c60c245932f8e3e18c65ed
Erickson Manuel Holguin <erickson01d@gmail.com>
2024-01-15 14:14:29 -0400

    Add InvitationSent component and handle invitation resend

--- src/app/signup/view/components/signup-form/index.tsx
+++ src/app/signup/view/components/signup-form/index.tsx
@@ -1,15 +1,46 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
+import { Stack } from "@chakra-ui/react";
+
+import { ContinueWithSocialMediaButtons } from "@/app/signin/view/components";
+import { OrDivider } from "@/components/atoms/OrDivider";
+import { TitleLoginComponent } from "@/components/atoms/TitleLoginComponent";
+
+import { InvitationSent } from "../invitation-sent";
 import { SignupHolder } from "./signup-form-holder";
 
 export const SignupFormContainer = () => {
+  const [invitationSent, setInvitationSent] = useState(false);
+
+  async function handleResendInvitation() {}
+
   return (
-    <SignupHolder
-      onSubmit={async (values) => {
-        console.log(values);
-      }}
-    />
+    <Stack spacing="6">
+      <TitleLoginComponent
+        description={
+          invitationSent
+            ? "Te hemos enviado una invitación a tu correo electrónico para que puedas completar tu registro."
+            : "Crea tu cuenta para poder acceder a todos los beneficios de la plataforma."
+        }
+        title={invitationSent ? "¡Gracias por registrarte!" : "Crea tu cuenta"}
+      />
+
+      {invitationSent ? (
+        <InvitationSent onResendInvitation={handleResendInvitation} />
+      ) : (
+        <>
+          <SignupHolder
+            onSubmit={async (values) => {
+              console.log(values);
+              setInvitationSent(true);
+            }}
+          />
+          <OrDivider />
+          <ContinueWithSocialMediaButtons />
+        </>
+      )}
+    </Stack>
   );
 };
--- src/app/signup/view/index.tsx
+++ src/app/signup/view/index.tsx
@@ -1,26 +1,13 @@
 import React from "react";
 
-import { Stack } from "@chakra-ui/react";
-
-import { ContinueWithSocialMediaButtons } from "@/app/signin/view/components";
-import { OrDivider } from "@/components/atoms/OrDivider";
 import { ScreenAreaDelimiter } from "@/components/atoms/ScreenAreaDelimiter";
-import { TitleLoginComponent } from "@/components/atoms/TitleLoginComponent";
 
 import { SignupFormContainer } from "./components/signup-form";
 
 export const SignupView = () => {
   return (
     <ScreenAreaDelimiter maxWidth="480">
-      <Stack spacing="6">
-        <TitleLoginComponent
-          description="Crea tu cuenta para poder acceder a todos los beneficios de la plataforma."
-          title="Crea tu cuenta"
-        />
-        <SignupFormContainer />
-        <OrDivider />
-        <ContinueWithSocialMediaButtons />
-      </Stack>
+      <SignupFormContainer />
     </ScreenAreaDelimiter>
   );
 };
